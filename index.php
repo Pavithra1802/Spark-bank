b88e367ab024b925e736be25a1effce923094e17
Pavithra1802 <72208634+Pavithra1802@users.noreply.github.com>
2021-07-21 08:52:57 +0530

    Add files via upload
    The Sparks Foundation 
    #GRIPJULY21

---
+++ index.php
@@ -0,0 +1,61 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Banking System</title>
+  </head>
+
+  <body style="background-color: black;">
+  <header>
+  <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : #38adda;"><b>SPARK BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #38adda;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color :#38adda;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #38adda;"><b>Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
+
+<main>
+  <style>
+  
+  body{
+            background-image: url('img/violetnet.jpg');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }
+.header-text
+{
+    text-align: center;
+    margin-top: 200px;
+    font-size: 20px;
+    
+}
+</style>
+<section>
+            <div class="header-text">
+                <h1 style="color: #f7f1f8;">WELCOME</h1>
+                <h2 style="color: #f7f1f8;">Keep your bank account in your packet ☺</h2>
+            </div>
+        </section>
+    </main>
+    <footer class="text-center mt-5 py-2">
+            <p>&copy 2021.<b>SPARK BANK</b> <br>Pavithra L<br> Banking Foundation</p>
+</footer>
+  </body>
+</html>
